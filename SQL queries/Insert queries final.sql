070bd92ca937e4198b4aeadf23bc2bdb682c2cb6
Tims Kurskiev <loam0606@mail.ru>
2019-10-24 00:11:02 +0300

    Update Insert queries final.sql

--- SQL queries/Insert queries final.sql
+++ SQL queries/Insert queries final.sql
@@ -11,37 +11,6 @@ order by id, parent_id nulls first
 
 TRUNCATE app.catalog_level RESTART IDENTITY;
 
---//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--
---\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--
-
-WITH companiesinsertqte as (
-	INSERT INTO app.catalog_level(name, description)
-	SELECT name, description
-	FROM app.catalog
-	RETURNING id, name
-),
-
-aggregatesinsertqte as (
-	INSERT INTO app.catalog_level(parent_id, name, description)
-	SELECT qte.id, ca.name, ca.description
-	FROM app.catalog_aggregate as ca
-	INNER JOIN app.catalog as c ON ca.catalog_id = c.id
-	INNER JOIN companiesinsertqte as qte ON c.name = qte.name
-	RETURNING id, name, parent_id
-)
-
---SELECT * FROM aggregatesinsertqte;
-
-INSERT INTO app.catalog_level(parent_id, name, description)
-SELECT qte.id, cm.name, cm.description
-FROM app.catalog_model as cm
-INNER JOIN app.catalog_aggregate as ca ON cm.catalog_aggregate_id = ca.id
-INNER JOIN app.catalog as c ON ca.catalog_id = c.id
-INNER JOIN aggregatesinsertqte as qte ON ca.name = qte.name --AND qte.parent_id = (SELECT cl.id from app.catalog_level as cl WHERE cl.name = c.name)
-WHERE qte.parent_id = (SELECT cl.id from app.catalog_level as cl WHERE cl.name = c.name)
-
---INNER JOIN aggregatesinsertqte qte ON ca.name = qte.name
-
 --------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------
 --//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--//\\--\\//--
