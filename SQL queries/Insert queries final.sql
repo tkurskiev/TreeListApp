da0398965e532b4f821333c86ffe836bbb4b0f57
Tims Kurskiev <loam0606@mail.ru>
2019-10-24 00:15:00 +0300

    Update Insert queries final.sql

--- SQL queries/Insert queries final.sql
+++ SQL queries/Insert queries final.sql
@@ -73,8 +73,4 @@ INNER JOIN app.catalog_aggregate as ca ON cm.catalog_aggregate_id = ca.id
 INNER JOIN app.catalog as c ON ca.catalog_id = c.id 
 INNER JOIN (SELECT * FROM app.catalog_level WHERE parent_id IS NOT NULL) as cl ON cl.name = ca.name
 	AND cl.parent_id = (SELECT id FROM app.catalog_level WHERE name = c.name);
-	
-	
---INNER JOIN app.catalog_level as cl ON cl.name = ca.name
-	--AND cl.parent_id = (SELECT id FROM app.catalog_level WHERE name = c.name)
 
